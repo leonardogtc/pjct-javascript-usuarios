e9aeefcfffe4f90caefa01853343fe6a8d679b0b
Leonardo G.T. da Conceição <leonardogtc@gmail.com>
2025-01-20 12:49:09 -0300

    Exercício do ForEach

---
+++ teste.js
@@ -0,0 +1,10 @@
+let cursos = [
+    "Curso completo de JavaScript",
+    "Curso completo de PHP",
+    "Curso completo de Desenvolvedor Web",
+    "Dominando o Banco de Dados MySQL"
+];
+
+cursos.forEach((curso, index) => {
+    console.log(curso)
+})
