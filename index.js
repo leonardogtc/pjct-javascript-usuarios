dc60e9ae049893403e5c4265f236e8a728e5a0cf
Leonardo G.T. da Conceição <leonardogtc@gmail.com>
2025-01-22 07:00:10 -0300

    Trabalhando com eventos

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
+const { preventOverflow } = require("@popperjs/core");
+
 var fields = document.querySelectorAll("#form-user-create [name]");
-var user = {}; 
+var user = {};
 
-fields.forEach(function(field, index){
 
-    if (field.name === "gender") {
 
-        if (field.checked) {
+console.log(user);
+
+
+// Captura o clique de todos os eventos de botão
+// document.querySelectorAll("button").forEach(function () {
+    // this.addEventListener("click", function(){
+    //     console.log("clicou!");
+    // });
+// });
+
+// document.getElementById('form-user-create').addEventListener('submit', function(){
+    // O console pisca rapidamente pois o botão submit executa refresh no formulário
+    // após sua utilização...
+    // console.log("Clicou no botão salvar!");
+    // alert('Cliquei no botão Salvar!');
+
+    // Cancelar o comportamento padrão do formulário
+    // event.preventDefault();
+    // console.log("Clicou no botão Salvar!");
+// });
+
+// Captura os dados do formulário
+document.getElementById("form-user-create").forEach(function() {
+    
+    event.preventDefault();
+
+    fields.forEach(function (field, index) {
+
+        if (field.name === "gender") {
+    
+            if (field.checked) {
+                user[field.name] = field.value
+            }
+    
+        } else {
+    
             user[field.name] = field.value
+    
         }
-
-    } else {
-
-        user[field.name] = field.value
-
-    }
-
+    
+    });
 });
-
-console.log(user)
